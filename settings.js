0fbd28471a0968a11be4615d731003f72a5b9175
Big Boss <138300692+bigbosssunzy@users.noreply.github.com>
2026-07-22 09:42:37 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,17 +1,17 @@
 const settings = {
   prefix: ".", // 👈 This stays as a property inside the object
-  packname: '🤡𝙄 𝙖𝙢 𝙟𝙤𝙠𝙚𝙧!🤡',
+  packname: '🤡🃏𝐈 𝐀𝐌 𝐉𝐎𝐊𝐄𝐑🃏🤡',
   author: '‎',
-  botName: "🤡𝙄 𝙖𝙢 𝙟𝙤𝙠𝙚𝙧!🤡",
+  botName: "🤡🃏𝐈 𝐀𝐌 𝐉𝐎𝐊𝐄𝐑🃏🤡",
   botOwner: '🦊⃟ᴠͥɪͣᴘͫ✮⃝🇧𝖎𝖌🇧ө͜͡ss𝄟⃝🎧™',
-  ownerNumber: '2347086057694',
+  ownerNumber: '234708605769',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.6",
-  updateZipUrl: "",
+  updateZipUrl: "https://github.com/bigbosssunzy/JOKER-MD/archive/refs/heads/main.zip",
 };
 
 // 👇 ADD THIS LINE HERE (Outside the curly braces)
